c16995adaa642ba6acbd6d56aa8ca28c98991f9d
Documenter.jl <documenter@juliadocs.github.io>
2022-07-28 17:43:50 +0000

    build based on 5ae4b82

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BareissFactorization","category":"page"},{"location":"#BareissFactorization","page":"Home","title":"BareissFactorization","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BareissFactorization.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BareissFactorization]","category":"page"}]
+}
