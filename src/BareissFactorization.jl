32b6f5c7e029a29b65b25b47a88950119431672f
Yingbo Ma <mayingbo5@gmail.com>
2022-07-28 20:25:31 -0400

    Add gaxpy! for efficient update of x2

--- src/BareissFactorization.jl
+++ src/BareissFactorization.jl
@@ -50,6 +50,54 @@ end
 
 find_pivot(x) = findfirst(!iszero, x)
 
+function gaxpy!(A, ds, x::AbstractVector, y::AbstractVector, tmp)
+    require_one_based_indexing(A, x, y, tmp)
+    m, n = size(A)
+    n == 0 && return y
+    if (m != length(y) || n != length(x) || n != length(ds) || length(ds) > length(tmp))
+        throw(DimensionMismatch(""))
+    end
+    p = first(ds)
+    for i in 1:min(2, n)
+        tmp[i] = one(p)
+    end
+    for i in 3:n
+        tmp[i] = p
+        p *= ds[i - 1]
+    end
+    p_nolast = n == 1 ? one(p) : p
+    p = last(ds)
+    for i in n-1:-1:1
+        tmp[i] *= p
+        if i != 1
+            p *= ds[i]
+        end
+    end
+    ll = similar(ds)
+    for i in 1:n
+        p = one(p)
+        for j in 1:n
+            (j == i || j == i - 1) && continue
+            p *= ds[j]
+        end
+        ll[i] = p
+    end
+    @assert ll == @view tmp[1:n]
+    for j in axes(A, 2)
+        xj = -tmp[j] * x[j]
+        for i in axes(A, 1)
+            y[i] += A[i, j] * xj
+        end
+    end
+    #@show A, ds, x, y, p_nolast
+    for i in eachindex(y)
+        yi, r = divrem(y[i], p_nolast)
+        @assert iszero(r)
+        y[i] = yi
+    end
+    y
+end
+
 function left_looking_bareiss(A; find_pivot = find_pivot)
     Base.require_one_based_indexing(A)
     m, n = size(A)
@@ -58,6 +106,7 @@ function left_looking_bareiss(A; find_pivot = find_pivot)
     L = zeros(T, m, m)
     U = zeros(T, m, n)
     ds = zeros(Rational{T}, m)
+    tmp = zeros(T, max(0, m - 1))
     ps = zeros(T, m)
     dp = 1
     for k in 1:n
@@ -68,7 +117,10 @@ function left_looking_bareiss(A; find_pivot = find_pivot)
         Pb = P * A[:, k]
         #x = T.(Ll \ (Pb))
         x1 = scaled_ldiv!(LowerTriangular(L[1:k-1, 1:k-1]), ps, Pb[1:k-1])
-        x2 = Pb[k:m] * dp - Int.(L[k:m, 1:k-1] * Diagonal(@view ds[1:k-1]) * x1 * dp)
+        #x2 = Pb[k:m] * dp - Int.(L[k:m, 1:k-1] * Diagonal(@view ds[1:k-1]) * x1 * dp)
+        x2 = gaxpy!(L[k:m, 1:k-1], (@view ps[1:k-1]), x1, zeros(T, m - k + 1), tmp) + dp * Pb[k:m]
+
+        #@show x2′ - x2
 
         U[1:k-1, k] = x1
         i = find_pivot(x2)
